2f74490b26f608c7afb13f3aeea02a15735b60b5
itsUdit24 <uditpradhan.official@gmail.com>
2025-04-22 16:06:32 +0530

    perfect number done ✅

---
+++ Basics/Math/perfectNum.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    /* Function to find whether the
+    number is perfect or not */
+    bool isPerfect(int n) {
+        // Edge case
+        if(n == 1) return false;
+        
+        /* Variable to store the sum
+        of all proper divisors */
+        int sum = 0;
+        
+        // Loop from 1 to square root of n
+        for(int i=1; i <= sqrt(n); ++i) {
+             
+            // Check if i is a proper divisor
+            if(n % i == 0){
+                // Update sum
+                sum = sum + i;
+                
+                /* Add the counterpart divisor
+                if it's different from i and
+                if it is not n itself */
+                if(n/i != n && i != n/i) {
+                    sum = sum + (n/i);
+                }
+            }
+        }
+        
+        // Compare sum and n
+        if(sum == n) return true;
+        return false;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number:  ";
+    cin >> n;
+    
+    /* Creating an instance of 
+    Solution class */
+    Solution sol; 
+    
+    /* Function call to find whether the
+     given number is perfect or not */
+    bool ans = sol.isPerfect(n);
+    
+    if(ans) {
+        cout << n << " is a perfect number." << endl;
+    } else {
+        cout << n << " is not a perfect number." << endl;
+    }
+    
+    return 0;
+}
